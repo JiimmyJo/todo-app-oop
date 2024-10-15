26264ab1cd1de3639072c571338dfa31ff584434
Jimmy Johansson <jimmyjohansson@outlook.com>
2024-10-15 14:58:19 +0200

    description

--- Commands/HelpCommand.cs
+++ Commands/HelpCommand.cs
@@ -3,8 +3,7 @@ public class HelpCommand
     public static void Execute()
     {
         Console.WriteLine("Help commands:");
-        Console.WriteLine("help");
-        Console.WriteLine("create-task");
+        Console.WriteLine("create-task - Create a task");
         Console.WriteLine("remove-task");
         Console.WriteLine("search-task");
     }
