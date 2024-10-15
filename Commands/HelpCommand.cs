a664d6c73ca53c60db74a088721bb9457197f3e9
Aeezin <gullestrand@gmail.com>
2024-10-15 14:57:14 +0200

    fix

--- Commands/HelpCommand.cs
+++ Commands/HelpCommand.cs
@@ -3,8 +3,9 @@ public class HelpCommand
     public static void Execute()
     {
         Console.WriteLine("Help commands:");
+        Console.WriteLine("help");
         Console.WriteLine("create-task");
         Console.WriteLine("remove-task");
-        Console.WriteLine("seatch-task");
+        Console.WriteLine("search-task");
     }
 }
